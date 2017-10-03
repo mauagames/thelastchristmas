a46de6b50b9dfeb5cb32dbe7738db43bf34b419c
lcr073 <lcr073@live.com>
2017-10-03 15:00:42 -0300

    Condicao Inicial
    Funcionamento parcial de elementos de animacao, introducao de pulo duplo, movimentação basica implementada, inicio da implementacao de elementos de joystick e de uma forca contraria ao movimento na planajem

---
+++ engenharia/mov/Proj/Luciano/Vascoreon/Assets/Scripts/Movimentacao.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class Movimentacao : MonoBehaviour {
+
+    public float velocidade = 9f;
+    public float fpulo = 3;
+    private Rigidbody2D rb;
+    private Animator anim;
+    public bool podePular = false;
+    public bool duploPulo = true;
+
+    // Use this for initialization
+    void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       /* if(Input.GetKey( KeyCode.RightArrow))
+        {
+            this.transform.Translate(new Vector3(velocidade * Time.deltaTime, 0, 0));
+            print("Direita");
+        }
+        */
+
+        float h = Input.GetAxis("Horizontal");
+     //   float v = Input.GetAxis("Vertical");
+            this.transform.Translate(new Vector3(h * velocidade * Time.deltaTime,0.0f, 0));
+
+       
+        if(h > 0.0f)
+        {
+            anim.ResetTrigger("idle");
+            anim.SetTrigger("correndo");
+        }
+        
+        else if(h< 0.0f)
+        {
+            anim.ResetTrigger("idle");
+            this.GetComponent("Sprite Render");
+            anim.SetTrigger("correndo");
+        }
+        else
+        {
+            //this.gameObject.GetComponent<SpriteRenderer>.;
+            anim.ResetTrigger("idle");
+            // anim.SetTrigger("correndo");
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space)) && (podePular))
+        {
+            rb.AddForce(new Vector2(0.0f, fpulo), ForceMode2D.Impulse);
+            anim.SetTrigger("pulo");
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space)) && (duploPulo) && (!podePular))
+        {
+            rb.AddForce(new Vector2(0.0f, fpulo), ForceMode2D.Impulse);
+            duploPulo = false;
+            anim.ResetTrigger("idle");
+            anim.SetTrigger("planando");
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.
+            rb.AddForce(new Vector2(0.0f, 0.5f), ForceMode2D.Impulse);
+
+        }
+        // this.transform.Rotate(new Vector3(0, 0, this.velocidade));
+    }
+
+    void OnCollisionEnter2D(Collision2D outro)s
+    {
+        if (outro.gameObject.CompareTag("chao"))
+        {
+            podePular = true;
+            duploPulo = true;
+         }
+
+
+        anim.SetTrigger("idle");
+        anim.ResetTrigger("planando");
+        print(outro.gameObject.tag);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D outro)
+    {
+        if (outro.gameObject.CompareTag("caixa"))
+        {
+            outro.gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D outro)
+    {
+        if (outro.gameObject.CompareTag("chao"))
+        {
+            podePular = false;
+
+        }
+
+
+
+    }
+}
---
+++ engenharia/mov/Proj/Luciano/Vascoreon/Assets/Scripts/gatilho1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gatilho1 : MonoBehaviour {
+    public bool podeRotacionar;
+    public GameObject objDeRotacao;
+    public float velRot = 3000;
+
+	// Use this for initialization
+	void Start () {
+        podeRotacionar = false;
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (podeRotacionar == true)
+        {        
+                objDeRotacao.transform.Rotate(new Vector3 (0.0f, 0.0f, velRot * Time.deltaTime));
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D outro)
+    {
+        if (outro.gameObject.CompareTag("Vascolina"))
+        {
+            podeRotacionar = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D outro)
+    {
+        if (outro.gameObject.CompareTag("Vascolina"))
+        {
+            podeRotacionar = false;
+        }
+    }
+}
