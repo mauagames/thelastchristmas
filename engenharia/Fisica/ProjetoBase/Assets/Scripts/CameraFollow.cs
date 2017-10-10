987953cb483399b995ecb79052afdf9b06999f1f
thomazerlach <thomazerlach@hotmail.com>
2017-10-10 15:11:34 -0300

    inicio de trocar de player (bugado)

--- engenharia/Fisica/ProjetoBase/Assets/Scripts/CameraFollow.cs
+++ engenharia/Fisica/ProjetoBase/Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 	public GameObject player;
+	public Vector2 ajuste;
 
 	void LateUpdate () {
-		this.transform.SetPositionAndRotation (new Vector3(player.transform.position.x, this.transform.position.y, this.transform.position.z), player.transform.rotation);
+		this.transform.SetPositionAndRotation (new Vector3(player.transform.position.x + ajuste.x, this.transform.position.y + ajuste.y, this.transform.position.z), player.transform.rotation);
+	}
+
+	public void setPlayer(GameObject pl) {
+		this.player = pl;
 	}
 }
